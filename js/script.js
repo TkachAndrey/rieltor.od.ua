59c406c22a973160a9f67eb9840cc73ee4f6f0b1
Project 1 <Paladin@MacBook-Air-Andrii.local>
2018-05-02 18:07:19 +0300

    section contacts

--- js/script.js
+++ js/script.js
@@ -9,26 +9,13 @@ $(document).ready(function(){
    // 2gis map
 
    DG.then(function() {
-       map = DG.map('map', {
+      map = DG.map('map', {
            'center': [46.447667, 30.746087],
            'zoom': 16
-       });
+      });
+
+      DG.marker([46.447804, 30.745851]).addTo(map).bindPopup('Мы здесь!');
    });
-   var myIcon = DG.icon({
-    iconUrl: 'my-icon.png',
-    iconRetinaUrl: 'my-icon@2x.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'my-icon-shadow.png',
-    shadowRetinaUrl: 'my-icon-shadow@2x.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-   });
-
-
-   DG.marker([46.447667, 30.746087], {icon: myIcon}).addTo(map).bindPopup('Мы здесь!');
-
 
 });
 
