3032616de8c18b982dddb15dc0268b42e47d24ad
Project 1 <Paladin@MacBook-Air-Andrii.local>
2018-05-02 14:49:10 +0300

    with 2gis map
    without adaptive

--- js/script.js
+++ js/script.js
@@ -5,4 +5,30 @@ $(document).ready(function(){
   	$(target).toggleClass("nav__list--open");
   });
 
+
+   // 2gis map
+
+   DG.then(function() {
+       map = DG.map('map', {
+           'center': [46.447667, 30.746087],
+           'zoom': 16
+       });
+   });
+   var myIcon = DG.icon({
+    iconUrl: 'my-icon.png',
+    iconRetinaUrl: 'my-icon@2x.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: 'my-icon-shadow.png',
+    shadowRetinaUrl: 'my-icon-shadow@2x.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+   });
+
+
+   DG.marker([46.447667, 30.746087], {icon: myIcon}).addTo(map).bindPopup('Мы здесь!');
+
+
 });
+
